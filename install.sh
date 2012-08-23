24949e5284e30916c840fcfa4e6e263006aae7a2
Onur Uyar <fluxproject@gmail.com>
2012-08-23 16:01:53 +0100

    install script fixed

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 cd $app_dir
+rvm gemset use campanify
 heroku accounts:set campanify_tech
-rvm getset use campanify
 git remote rm heroku
 git remote add heroku git@heroku.campanify_tech:$slug.git           
+bundle
+git add . 
 git commit -am 'seeds.rb updated'
 git push heroku master
-bundle exec rake campanify:setup
+bundle exec rake campanify:setup --trace
