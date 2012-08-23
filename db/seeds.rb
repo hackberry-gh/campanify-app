d0ed5c52aa5ee6a513d39c69d13d252f290ba62c
Onur Uyar <fluxproject@gmail.com>
2012-08-23 14:25:53 +0100

    seed templetazied

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-campaign = Campaign.create(name: "Campanify Base")
-campaign.slug = "campanify-base"
+campaign = Campaign.create(name: "$name")
+campaign.slug = "$slug"
 campaign.save!
-campaign.setup
+campaign.setup if Rails.env.production?
 
-admin = Administrator.create!(email: 'admin@campanify.it', full_name: "Admin", role: "root")
-admin.password = "passw0rd"
-admin.password_confirmation = "passw0rd"
+admin = Administrator.create!(email: '$admin_email', full_name: "$admin_full_name", role: "root")
+admin.password = "$admin_password"
+admin.password_confirmation = "$admin_password"
 admin.save!
 
 user = User.create!(first_name: "John", last_name: "Doe", email: "johndoe@campanify.it")
@@ -15,10 +15,20 @@ about = Content::Page.create!(title: "About", body: File.read("#{Rails.root}/db/
 thank_you = Content::Page.create!(title: "Thank You", body: File.read("#{Rails.root}/db/seeds/pages/thank-you.html"))
 user_form = Content::Widget.create!(title: "User Form", body: File.read("#{Rails.root}/db/seeds/widgets/user-form.html.erb"))
 social_sharing = Content::Widget.create!(title: "Social Sharing", body: File.read("#{Rails.root}/db/seeds/widgets/social-sharing.html.erb"))
+flexi_slider = Content::Widget.create!(title: "Flexi Slider", body: File.read("#{Rails.root}/db/seeds/widgets/flexi-slider.html.erb"))
 
 home.widgets << user_form
 thank_you.widgets << social_sharing
+about.widgets << flexi_slider
 
 sample_post = Content::Post.new(title: "Sample Post", body: File.read("#{Rails.root}/db/seeds/posts/sample.md"))
 sample_post.user = user
 sample_post.save!
+
+sample_event = Content::Event.create(
+name: "Campanify Launch Party", description: "We are proud to launch our baby Campanify 1.0",
+start_time: Time.now, location: "Whitecube Gallery", 
+venue: {street: "Hoxton Square", city: "London", state: "England", zip: "N1 6PB", 
+  country: "United Kingdom", latitude: "51.52725461288175", longitude: "-0.0813526878051789" },
+privacy: "OPEN"  
+)
--- install.sh
+++
@@ -1,3 +0,0 @@
-heroku create campanify-base
-git push heroku master
-bundle exec rake campanify:setup
