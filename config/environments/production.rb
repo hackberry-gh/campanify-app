a0d253f05b3b56267e990b190b081723055bd642
Onur Uyar <fluxproject@gmail.com>
2013-03-06 03:06:24 +0000

    jquery hell

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Campanify::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w(
+  vendor/jquery.js
   vendor/jquery.sparkline.js 
   vendor/modernizr.js 
   admin/active_admin.js admin/active_admin.css
